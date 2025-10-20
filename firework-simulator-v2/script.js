0dc24d32b5c99354da9e2a00413378d56f7d8aea
Nandu Kumar Koribilli <83496495+Nandukumar-koribilli@users.noreply.github.com>
2025-10-20 19:54:28 +0530

    Add files via upload

--- firework-simulator-v2/script.js
+++ firework-simulator-v2/script.js
@@ -801,7 +801,6 @@ const shellNames = Object.keys(shellTypes);
 
 function init() {
 	// Remove loading state
-	document.querySelector('.loading-init').remove();
 	appNodes.stageContainer.classList.remove('remove');
 	
 	// Populate dropdowns
@@ -2283,26 +2282,19 @@ const soundManager = {
 
 // Kick things off.
 
-function setLoadingStatus(status) {
-	document.querySelector('.loading-init__status').textContent = status;
-}
-
 // CodePen profile header doesn't need audio, just initialize.
 if (IS_HEADER) {
 	init();
 } else {
-	// Allow status to render, then preload assets and start app.
-	setLoadingStatus('Lighting Fuses');
-	setTimeout(() => {
-		soundManager.preload()
-		.then(
-			init,
-			reason => {
-				// Codepen preview doesn't like to load the audio, so just init to fix the preview for now.
-				init();
-				// setLoadingStatus('Error Loading Audio');
-				return Promise.reject(reason);
-			}
-		);
-	}, 0);
+	// Preload assets and start app.
+	soundManager.preload()
+	.then(
+		init,
+		reason => {
+			// Codepen preview doesn't like to load the audio, so just init to fix the preview for now.
+			init();
+			// setLoadingStatus('Error Loading Audio');
+			return Promise.reject(reason);
+		}
+	);
 }
